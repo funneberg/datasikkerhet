a90612849a5e7ffeecbfbbfb61b882d9d1b8de18
larsmra <43782654+larsmra@users.noreply.github.com>
2020-01-15 23:35:41 +0100

    laget logginn- og registreringsside i appen.

--- datasikkerhetApp/app/src/main/java/com/example/datasikkerhetapp/MainActivity.java
+++ datasikkerhetApp/app/src/main/java/com/example/datasikkerhetapp/MainActivity.java
@@ -2,33 +2,38 @@ package com.example.datasikkerhetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import java.net.HttpURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
         //HttpURLConnection connection =
 
-        Button btnLogIn = findViewById(R.id.btnLogIn);
-        final TextView textView = findViewById(R.id.txtTest);
-
-
-        btnLogIn.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                CharSequence charSequence = "o o f";
-                textView.setText(charSequence);
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
 
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
 
     }
 }
+
