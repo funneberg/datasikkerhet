deb0dd0693d907828846449926cccab952831703
larsmra <43782654+larsmra@users.noreply.github.com>
2020-05-06 21:51:38 +0200

    Nyeste filene

--- nettside_ver2/view/LoginView.php
+++ nettside_ver2/view/LoginView.php
@@ -9,6 +9,7 @@ class LoginView extends View {
      * Viser login-siden.
      */
     public function output(Model $model): void {
+
         // Hvis man allerede er logget inn, har man ikke tilgang til denne siden.
         if (isset($_SESSION['loggedIn'])) {
             header("location: index.php");
